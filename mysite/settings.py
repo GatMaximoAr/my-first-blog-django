ac04b32f2541e13347d57f3da587f1553376a851
GatMaximoAr <gaticamaxiimo2@gmail.com>
2023-03-13 00:17:21 -0300

    try fix mime css error 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
